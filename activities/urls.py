5a45f35495040576cc314d749e3db91e7066b7c8
Katherine Michel <kthrnmichel@gmail.com>
2021-09-01 21:28:59 -0500

    Add API docs

--- activities/urls.py
+++ activities/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
 
+from rest_framework.schemas import get_schema_view
+
 from activities.views import HomePageView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
+    path(
+        "openapi",
+        get_schema_view(
+            title="Nursing Home Activities API",
+            description="API for activities and residents",
+            version="1.0.0",
+        ),
+        name="openapi-schema",
+    ),
 ]
