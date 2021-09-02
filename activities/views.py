01b96046c34c586885a90d3bed764f98ae0cfdc3
Katherine Michel <kthrnmichel@gmail.com>
2021-09-01 22:01:39 -0500

    Adding resident filter

--- activities/views.py
+++ activities/views.py
@@ -24,6 +24,12 @@ class ActivityFilter(filters.FilterSet):
         fields = ["start"]
 
 
+class ResidentFilter(filters.FilterSet):
+    class Meta:
+        model = Resident
+        fields = ["activity__id", "status"]
+
+
 class IndexView(TemplateView):
 
     template_name = "index.html"
@@ -39,6 +45,20 @@ class ActivityViewSet(viewsets.ModelViewSet):
 class ResidentViewSet(viewsets.ModelViewSet):
     queryset = Resident.objects.all()
     serializer_class = ResidentSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = ResidentFilter
+    # filter_fields = ("activity__id", "status")
+
+    # http://127.0.0.1:8000/residents/?activity__id=1&status=going
+
+    """
+    def get_queryset(self):
+        queryset = Activity.objects.all()
+        activity = self.request.query_params.get("activity__id")
+        status = self.request.query_params.get("status")
+        if activity is not None:
+            queryset = queryset.filter(activity=activity, status=status)
+    """
 
 
 class UserViewSet(viewsets.ModelViewSet):
