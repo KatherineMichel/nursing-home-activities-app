d6d334c43bb1fab0957ab14fc2130acc885cf56d
Katherine Michel <kthrnmichel@gmail.com>
2021-09-01 21:40:30 -0500

    Add user viewset

--- activities/urls.py
+++ activities/urls.py
@@ -4,9 +4,11 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 from rest_framework.schemas import get_schema_view
 
+from activities import views
 from activities.views import HomePageView
 
 router = DefaultRouter()
+router.register(r"users", views.UserViewSet)
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
--- activities/views.py
+++ activities/views.py
@@ -1,6 +1,18 @@
+from django.contrib.auth.models import User
 from django.views.generic.base import TemplateView
+
+from rest_framework import viewsets
+
+from activities.serializers import (
+    UserSerializer,
+)
 
 
 class HomePageView(TemplateView):
 
     template_name = "home.html"
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
