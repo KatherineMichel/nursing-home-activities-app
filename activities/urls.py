a6746eac08388f906125be2bce702d23d8fb6621
Katherine Michel <kthrnmichel@gmail.com>
2021-09-01 21:30:24 -0500

    Add default router

--- activities/urls.py
+++ activities/urls.py
@@ -1,11 +1,16 @@
+from django.conf.urls import include
 from django.urls import path
 
+from rest_framework.routers import DefaultRouter
 from rest_framework.schemas import get_schema_view
 
 from activities.views import HomePageView
 
+router = DefaultRouter()
+
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
+    path("api/", include(router.urls)),
     path(
         "openapi",
         get_schema_view(
