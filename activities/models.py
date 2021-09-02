89a516852daae6ad45ae6ce3626682b4b37c71ba
Katherine Michel <kthrnmichel@gmail.com>
2021-09-01 21:22:01 -0500

    Adding activity models

--- activities/models.py
+++ activities/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+LOCATION_OPTIONS = [
+    ("activity room", "activity room"),
+    ("skilled nursing", "skilled nursing"),
+    ("assisted living", "assisted living"),
+    ("memory care", "memory care"),
+    ("dining room", "dining room"),
+    ("outing", "outing"),
+]
+
+ACTIVITY_OPTIONS = [
+    ("virtual", "virtual"),
+    ("in person", "in person"),
+    ("both", "both"),
+]
+
+STATUS_OPTIONS = [
+    ("going", "going"),
+    ("not going", "not going"),
+    ("maybe", "maybe"),
+]
+
+
+class Activity(models.Model):
+    name = models.CharField(max_length=100, default="")
+    description = models.CharField(max_length=255, blank=True, null=True, default="")
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    location = models.CharField(
+        choices=LOCATION_OPTIONS, default="dining room", max_length=100
+    )
+    options = models.CharField(choices=ACTIVITY_OPTIONS, default="both", max_length=100)
+    residents = models.ManyToManyField(
+        "Resident", blank=True, null=True, related_name="activities"
+    )
+
+
+class Resident(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
+    activity = models.ForeignKey(
+        Activity, on_delete=models.CASCADE, related_name="activity"
+    )
+    status = models.CharField(
+        choices=STATUS_OPTIONS, default="not going", max_length=100
+    )
