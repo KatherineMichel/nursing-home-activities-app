6b6e8c34e83672d4842785192faf361655020c78
Katherine Michel <kthrnmichel@gmail.com>
2021-09-02 23:42:44 -0500

    Add model tests

--- activities/tests.py
+++ activities/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from model_bakery import baker
 
-# Create your tests here.
+from .models import (
+    Activity,
+    Resident
+)
+
+
+class TestModel(TestCase):
+    def test_group_026_model(self):
+        activity = baker.make(Activity)
+        assert activity.name
+        assert activity.description
+        assert activity.start
+        assert activity.end
+        assert activity.location
+        assert activity.options
+        assert activity.residents
+
+    def test_group_028_model(self):
+        resident = baker.make(Resident)
+        assert resident.user
+        assert resident.activity
+        assert resident.status
